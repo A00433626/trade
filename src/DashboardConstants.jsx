3c9739b36400cf9d9c5ba0db59d8e853a0e8e46f
vtejakatragadda@perennia.ca+1-at-290587490288 <vtejakatragadda@perennia.ca>
2021-04-05 18:27:57 -0300

    change in route

--- src/DashboardConstants.jsx
+++ src/DashboardConstants.jsx
@@ -1,3 +1,3 @@
-export const APPLICATION_NAME = "/trade";
-export const DASHBOARD_URL = `${APPLICATION_NAME}/dashboard`;
-export const ACCOUNT_URL = `${APPLICATION_NAME}/accountsummary`;
+// export const APPLICATION_NAME = "/trade";
+export const DASHBOARD_URL = `dashboard`;
+export const ACCOUNT_URL = `accountsummary`;
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./LandingPage.css";
 import LogoutButton from "./LogoutButton";
-import { APPLICATION_NAME } from "../DashboardConstants.jsx";
+// import { APPLICATION_NAME } from "../DashboardConstants.jsx";
 import { withAuth0 } from "@auth0/auth0-react";
 class LandingPage extends Component {
 	render() {
@@ -11,9 +11,7 @@ class LandingPage extends Component {
 				{/* Account Logo */}
 				<div className="main-conatianer-header-logo">
 					{/* {user ?  */}
-					<a
-						href={`/${APPLICATION_NAME}/dashboard`}
-						style={{ textDecoration: "none" }}>
+					<a href={`dashboard`} style={{ textDecoration: "none" }}>
 						logo
 					</a>
 					{/* : "logo"} */}
@@ -22,7 +20,7 @@ class LandingPage extends Component {
 				{/* {user && ( */}
 				<div className="menu-icons">
 					<div className="menu-icons-anchor">
-						<a href={`/${APPLICATION_NAME}/accountsummary`}>Account</a>
+						<a href={`accountsummary`}>Account</a>
 					</div>
 					<div className="menu-icons-signout">
 						<LogoutButton />
